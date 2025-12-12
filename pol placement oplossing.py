6cecec37b5292be4ea35a7b05392b331e1589ba0
Python-man-persoon <eric.nusser@hva.nl>
2025-12-12 14:32:40 +0100

    Add files via upload
    pol placement opdracht

---
+++ pol placement oplossing.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 28 12:54:50 2025
+
+@author: ericn
+"""
+
+#import systems
+import wis_2_2_utilities as util
+import wis_2_2_systems as systems
+
+import control as ct
+import numpy as np
+import math
+
+#set timestep
+timestep = 2e-3
+
+g = 9.81
+
+#pendulum 
+rho_wood = 700 #kg/m3
+l = 0.6 #pendulum1_length
+d = 0.2
+mp=rho_wood * l * d**2
+print(mp)
+
+#flywheel 
+rho_iron = 7870 #kg/m3
+flywheel_radius = 0.1
+flywheel_thickness = 0.01 
+mc = rho_iron*(math.pi * flywheel_radius**2 * flywheel_thickness)
+Ic = 0.5 * mc * flywheel_radius**2
+print (mc)
+
+#hoekversenelling
+Icm = (1/12) * mp* (d**2 + l**2)
+I = Icm + (mp*(l/2)**2)
+Icyl = (1/2) * mc * flywheel_radius**2
+print (Icyl)
+print (Icm)
+print (I)
+
+#termen
+T1 = (I + mc *(l**2))
+T2 = ((mp * g * l)/(2 * (T1))) + ((mc * g * l)/(T1))
+
+matrix_A = np.array([[0,1,0,0],
+                     [0, 0, -T2, 0],
+                     [0,0,0,1],
+                     [0, 0, T2, 0]])
+
+matrix_B = np.array([[0],[(1/Icyl)],[0],[(-1/T1)]])
+
+print('eigenvalues of A are:')
+print(np.linalg.eigvals(matrix_A))
+
+
+list_poles =[-3, -6, -9, -12]
+matrix_K = ct.place(matrix_A,matrix_B,list_poles)
+print(' ')
+print('Gain matrix K is:')
+print(matrix_K)
+print(' ')
+print('eigenvalues of (A-BK) are:')
+print(np.linalg.eigvals((matrix_A-matrix_B @ matrix_K)))
+
+
+class controller():
+    def __init__(self, target=0):
+        self.matrix_gain=np.array([[-1.11700822e+00, -7.75700154e-01, -1.26469337e+03, -2.74726362e+02]])
+
+    def feedBack(self, observe): 
+        u= -self.matrix_gain @ observe
+        return u
+    
+def main():
+  model=systems.flywheel_inverted_pendulum()
+  control = controller()
+  simulation = util.simulation(model=model,timestep=timestep)
+  simulation.setCost()
+  simulation.max_duration = 10 #seconds
+  simulation.GIF_toggle = False #set to false to avoid frame and GIF creation
+
+  while simulation.vis.Run():
+      if simulation.time<simulation.max_duration:
+        simulation.step()
+        u = control.feedBack(simulation.observe())
+        simulation.control(u)
+        simulation.log()
+        simulation.refreshTime()
+      else:
+        print('Ending visualisation...')
+        simulation.vis.GetDevice().closeDevice()
+  print('Simulation cost end: ', simulation.cost_input, simulation.cost_state)      
+  simulation.writeData()
+  plot.csv()
+        
+
+if __name__ == "__main__":
+  main()
