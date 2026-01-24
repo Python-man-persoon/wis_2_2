c20ca935c555da0fcb8a2c93d2c4fa5a445f46d2
Finn-HvA <finn.van.woensel@hva.nl>
2026-01-24 15:33:12 +0100

    plot_csv aangepast voor 2 pendulum

--- project_3/plot_csv_3slinger.py
+++ project_3/plot_csv_3slinger.py
@@ -18,38 +18,51 @@ def csv():
     time = df[0]
 
     # --- Pendulum 1 ---
-    plt.subplot(3, 2, 1)
+    plt.subplot(4, 2, 1)
     plt.plot(time, df[3], color='b')
     plt.ylabel('Angle (rad)')
     plt.title('Pendulum 1: Angle')
     plt.grid(True)
 
-    plt.subplot(3, 2, 2)
+    plt.subplot(4, 2, 2)
     plt.plot(time, df[4], color='r')
     plt.ylabel('Speed (rad/s)')
     plt.title('Pendulum 1: Angular Speed')
     plt.grid(True)
 
     # --- Pendulum 2 ---
-    plt.subplot(3, 2, 3)
+    plt.subplot(4, 2, 3)
     plt.plot(time, df[5], color='b')
     plt.ylabel('Angle (rad)')
     plt.title('Pendulum 2: Angle')
     plt.grid(True)
 
-    plt.subplot(3, 2, 4)
+    plt.subplot(4, 2, 4)
     plt.plot(time, df[6], color='r')
     plt.ylabel('Speed (rad/s)')
     plt.title('Pendulum 2: Angular Speed')
     plt.grid(True)
 
+    # --- Pendulum 3 ---
+    plt.subplot(4, 2, 5)
+    plt.plot(time, df[7], color='b')
+    plt.ylabel('Angle (rad)')
+    plt.title('Pendulum 3: Angle')
+    plt.grid(True)
+
+    plt.subplot(4, 2, 6)
+    plt.plot(time, df[8], color='r')
+    plt.ylabel('Speed (rad/s)')
+    plt.title('Pendulum 3: Angular Speed')
+    plt.grid(True)
+
     # --- Control Input ---
     # Placing it in the 7th slot (bottom left)
-    plt.subplot(3, 2, 5)
-    plt.plot(time, df[1], color='g')
+    plt.subplot(4, 2, 7)
+    plt.plot(time, df[9], color='g')
     plt.xlabel('Time (s)')
     plt.ylabel('Input')
-    plt.title('Quadratic State Cost vs Time')
+    plt.title('Control Input vs Time')
     plt.grid(True)
 
     plt.tight_layout()
